f08bc0fa4a838dd0aa273bd351e363cf7db894d2
backminjoo <qoralswn1205@daum.net>
2023-07-22 22:36:20 +0900

    mbti-test

---
+++ src/main/java/com/example/setting/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package com.example.setting.repository;
+
+import com.example.setting.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+    Optional<MemberEntity> findByMemberEmail(String memberEmail);
+
+    boolean existsByMemberEmail(String memberEmail);
+
+    boolean existsByMemberNickname(String memberNickname);
+}
