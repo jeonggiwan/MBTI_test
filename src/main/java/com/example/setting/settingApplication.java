07d27a3cdc78ad32f35789bbd7cc6fb4e4d15900
backminjoo <qoralswn1205@daum.net>
2023-07-24 01:11:36 +0900

    폴더 이름 변경

--- src/main/java/com/example/setting/settingApplication.java
+++ src/main/java/com/example/setting/settingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class mbtiApplication {
+public class settingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(mbtiApplication.class, args);
+        SpringApplication.run(settingApplication.class, args);
     }
 
 }
